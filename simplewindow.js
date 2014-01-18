a80c104c30e4fe59f6c1c1e82e5999c0e5dd95cf
Elliot <elliot.j@live.se>
2014-01-18 17:47:46 +0100

    added a moon

--- simplewindow.js
+++ simplewindow.js
@@ -1,23 +1,36 @@
 
-function createCanvas() {
+// HTML Canvas Reference
+// http://www.w3schools.com/tags/ref_canvas.asp
+
+	
+
+function calibrateCanvas() {
 	var canvas = document.getElementById('paint');
 	var style = window.getComputedStyle(paint);
 	canvas.setAttribute('width', style.width);
 	canvas.setAttribute('height', style.height);
 }
 
-function drawSquare() {
+function drawBox() {
 	var paint = document.getElementById("paint").getContext("2d");
 
-	var x = 20;
-	var y = 20;
+	var x = 60;
+	var y = 30;
 
 	paint.moveTo(x, y);
 
-	paint.lineTo(x + 20, y);
-	paint.lineTo(x + 20, y + 20);
-	paint.lineTo(x, y + 20);
+	paint.lineTo(x + 50, y);
+	paint.lineTo(x + 50, y + 50);
+	paint.lineTo(x, y + 50);
 	paint.lineTo(x, y);
 
+	paint.stroke();
+}
+
+function drawMoon() {
+	var paint = document.getElementById("paint").getContext("2d");
+
+	paint.beginPath();
+	paint.arc(300, 400, 100, 0, 2*Math.PI);
 	paint.stroke();
 }
