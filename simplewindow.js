8f255c3d4d8a4b81c8cd07309c5c794c6404da7b
Elliot <elliot.j@live.se>
2014-01-18 21:53:39 +0100

    added support for turtles

--- simplewindow.js
+++ simplewindow.js
@@ -4,16 +4,18 @@
 // http://www.w3schools.com/tags/ref_canvas.asp 
 	
 
+
 function calibrateCanvas() {
 	var canvas = document.getElementById('paint');
 	var style = window.getComputedStyle(paint);
 	canvas.setAttribute('width', style.width);
 	canvas.setAttribute('height', style.height);
+
+	// initiate global paint (this function is called with onload)
+	paint = document.getElementById("paint").getContext("2d");
 }
 
 function drawBox() {
-	var paint = document.getElementById("paint").getContext("2d");
-
 	var x = 60;
 	var y = 30;
 
@@ -28,18 +30,19 @@ function drawBox() {
 }
 
 function drawMoon() {
-	var paint = document.getElementById("paint").getContext("2d");
-
 	paint.beginPath();
 	paint.arc(300, 400, 100, 0, 2*Math.PI);
 	paint.stroke();
 }
 
 function execute() {
-	var paint = document.getElementById("paint").getContext("2d");
 	var code = document.getElementById('code').value;
 
-	eval(code);
+	try {
+		eval(code);
+	} catch(err) {
+		alert('Syntax error');
+	}
 
 //	var temp = code.split(';');
 
@@ -51,4 +54,23 @@ function execute() {
 function clearCanvas() {
 	var canvas = document.getElementById('paint');
 	canvas.width = canvas.width;
+}
+
+function Turtle(x, y) {
+	this.x = x;
+	this.y = y;
+	this.direction = 90;
+}
+
+Turtle.prototype.forward = function(distance) {
+	paint.moveTo(this.x, this.y);
+	var rad = this.direction * Math.PI / 180;
+	this.x += distance * Math.cos(rad);
+	this.y -= distance * Math.sin(rad);
+	paint.lineTo(this.x, this.y);
+}
+
+Turtle.prototype.left = function(degrees) {
+	this.direction += degrees;
+	this.direction %= 360;
 }
